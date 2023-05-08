61319d606ef228163346c464d8ca09ae6b0201dd
bongholee <leebongho9204@gmail.com>
2023-05-09 07:52:50 +0900

    onNext, onError, onComplete 에 대한 처리를 파라미터로 전달받는 subscribe

--- src/main/java/com/bong/sec01/Lec03MonoSubscribe.java
+++ src/main/java/com/bong/sec01/Lec03MonoSubscribe.java
@@ -10,6 +10,15 @@ public class Lec03MonoSubscribe {
 
         // 1. 파라미터 없는 subscribe. Publisher(mono)는 데이터를 발행
         // 하지만 onNext, onError, onComplete 이벤트에 대한 처리가 없기 때문에 아무런 동작이 발생하지 않는 것 처럼 보임
-        mono.subscribe();
+        //mono.subscribe();
+
+        // 2. onNext, onError, onComplete 처리를 위한 매개변수를 받는 subscribe 메서드
+        // 내부적으로 Subscriber가 생성자 파라미터로 받아 생성된다.
+        // 즉, 아래 파라미터들은 Subscriber를 구성하는 파라미터들
+        mono.subscribe(
+                item -> System.out.println("received : " + item),
+                err -> System.out.println(err.getMessage()),
+                () -> System.out.println("Complete!!!")
+        );
     }
 }
